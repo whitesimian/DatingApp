4dbae0c646689a76143caeb24a81f0e7dac227c1
Felipe Machado <65lopesmachado@cua.edu>
2022-02-02 14:51:55 -0300

    Added angular page

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,28 +7,27 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
     {
-        private readonly DataContext _context;
+        _context = context;
+    }
 
-        public UsersController(DataContext context)
-        {
-            _context = context;
-        }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
+        return await _context.Users.ToListAsync();
+    }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
-            return await _context.Users.ToListAsync();
-        }
-
-        // api/users/{id}
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id) {
-            return await _context.Users.FindAsync(id);
-        }
+    // api/users/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id) {
+        return await _context.Users.FindAsync(id);
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -14,51 +14,55 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace API
+namespace API;
+
+public class Startup
 {
-    public class Startup
+    private readonly IConfiguration _config;
+    public Startup(IConfiguration config)
     {
-        private readonly IConfiguration _config;
-        public Startup(IConfiguration config)
+        _config = config;
+    }
+
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<DataContext>(options => { 
+            options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddControllers();
+        services.AddCors();  // Permite HTTP requests de fora do domínio de origem (5001).
+        services.AddSwaggerGen(c =>
         {
-            _config = config;
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
         }
 
+        app.UseHttpsRedirection();
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        app.UseRouting();
+
+        app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod()
+            .WithOrigins("https://localhost:4200"));
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
         {
-            services.AddDbContext<DataContext>(options => { 
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+            endpoints.MapControllers();
+        });
     }
 }
+
